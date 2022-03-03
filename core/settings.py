5f94d94ae1578a670b6890ceb1308d68330c2ad8
pvrvlapplication <Pvrvl@2019>
2022-03-03 19:28:43 +0530

    Second Commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(m!=^%ue==s5ly*y%!k9g97f^+01+f&mrv15@rput^nroprz@i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','pvrvl-heroku.herokuapp.com']
 
 
 # Application definition
